54e02f6f5b855178d741686579e004a06f2dfb34
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2024-09-26 22:15:59 +0600

    fixed some logic

--- scripts/data.js
+++ scripts/data.js
@@ -9,7 +9,7 @@ const cardData = (data) => {
   const cardHead = document.getElementById("card-head");
   const noCardhead = document.getElementById("no-card-head");
 
-  if (Array.isArray(data)) {
+  if (Array.isArray(data) && data.length > 0 && data[0]) {
     for (const item of data) {
       const card = document.createElement("div");
       card.classList.add("card");
@@ -35,6 +35,6 @@ const cardData = (data) => {
           <h1 class="no-card">No Item Has Been Found.</h1>
         </div>
     `;
-    noCardhead.appendChild(card);
+    noCardhead.appendChild(noCard);
   }
 };
